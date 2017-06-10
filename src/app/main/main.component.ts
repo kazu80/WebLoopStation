7205b83211a252a8a858532ca9e694eb39d6e433
monkick <saruyama.monki@gmail.com>
2017-06-10 15:55:47 +0900

    stream control play or stop

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -18,13 +18,55 @@ export class MainComponent implements OnInit, AfterViewInit, DoCheck {
   public rec = false;
   public time = 0;
   public beat: number;
-  public mediaRecorder: any;
+  mediaRecorder: any;
+  public recordedChunks: any[] = [];
 
   constructor(private service_recording: RecordingService,
               private service_user_media: UsermediaService) {
   }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+
+    // Get User Media
+    navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(stream => {
+      this.mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'});
+      this.mediaRecorder.ondataavailable = (event: any) => {
+        if (event.data.size > 0) {
+          this.recordedChunks.push(event.data)
+        }
+      };
+
+      this.service_recording.stream = stream;
+      this.service_user_media.user_media = true;
+    });
+  }
+
+  /**
+   * 変更チェックをngDoCheckで行なう。
+   * AngularJS/Vueで言うところのwatch
+   */
+  ngDoCheck() {
+    if (this.service_user_media.user_media) {
+
+      if (!this.service_recording.recording && this.service_recording.start) {
+        this.mediaRecorder.start();
+        this.service_recording.start = false;
+      } else if (this.service_recording.recording && this.service_recording.stop) {
+        this.mediaRecorder.stop();
+        this.service_recording.stop = false;
+
+        const superBuffer: Blob = new Blob(this.recordedChunks);
+        const recordingSoundUrl: string = window.URL.createObjectURL(superBuffer);
+        console.log(recordingSoundUrl);
+      }
+    }
+  }
+
+  public click(): void {
     const stream = new Observable(observer => {
       let beat = 0;
       const interval = setInterval(() => {
@@ -48,43 +90,15 @@ export class MainComponent implements OnInit, AfterViewInit, DoCheck {
           if (value[1] % 4 === 0) {
             this.time++;
             this.service_recording.recording = !this.service_recording.recording;
+
+            if (this.service_recording.recording) {
+              this.service_recording.start = true;
+            } else {
+              this.service_recording.stop = true;
+            }
           }
 
           console.log(this.play, this.beat, this.time);
         });
-  }
-
-  ngAfterViewInit() {
-    navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(this.handleSuccess);
-
-  }
-
-  /**
-   * 変更チェックをngDoCheckで行なう。
-   * AngularJS/Vueで言うところのwatch
-   */
-  ngDoCheck() {
-    if (this.service_recording.recording) {
-      if (this.service_user_media.user_media) {
-
-
-      }
-    }
-
-
-  }
-
-  private handleSuccess (stream): void {
-    console.log("get user media success!");
-
-    /*
-     this.mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'});
-     this.mediaRecorder.ondataavailable = (event: any) => {
-     if (event.data.size > 0) this.recordedChunks.push(event.data);
-     };
-     */
-
-    this.service_recording.stream = stream;
-    this.service_user_media.user_media = true;
   }
 }
--- src/app/service/recording.service.ts
+++ src/app/service/recording.service.ts
@@ -2,18 +2,21 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class RecordingService {
-  private _recording: any = false;
+    private _recording: any = false;
     private _stream: any;
+    private _start: any = false;
+    private _stop: any = false;
 
-  constructor() { }
+    constructor() {
+    }
 
-  get recording(): boolean {
-    return this._recording;
-  }
+    get recording(): boolean {
+        return this._recording;
+    }
 
-  set recording(value: boolean) {
-    this._recording = value;
-  }
+    set recording(value: boolean) {
+        this._recording = value;
+    }
 
     get stream(): any {
         return this._stream;
@@ -23,4 +26,19 @@ export class RecordingService {
         this._stream = value;
     }
 
+    get start(): any {
+        return this._start;
+    }
+
+    set start(value: any) {
+        this._start = value;
+    }
+
+    get stop(): any {
+        return this._stop;
+    }
+
+    set stop(value: any) {
+        this._stop = value;
+    }
 }
